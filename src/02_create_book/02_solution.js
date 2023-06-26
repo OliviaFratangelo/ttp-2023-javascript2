66551a213679a5e38271a75b6edc29dc1f7a9e60
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 19:20:39 -0400

    trying to add rating

--- src/02_create_book/02_solution.js
+++ src/02_create_book/02_solution.js
@@ -1,12 +1,13 @@
 // Code your solution here
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-function createBook(id, title, author, price) {
+function createBook(id, title, author, price, rating) {
   let Book = Object.create(createBook);
 
   Book.id = id;
   Book.title = title;
   Book.author = author;
   Book.price = price;
+  Book.rating = [];
   return Book;
 }
 
@@ -18,6 +19,11 @@ createBook.getPrice = function () {
   return `${this.price}`;
 };
 
-let bookCreate = new createBook(1, 'Wonder', 'R.J. Palacio', '$10');
+createBook.addRating = function (rating) {
+  this.rating.push(rating);
+};
+
+let bookCreate = new createBook(1, 'Wonder', 'R.J. Palacio', '$10', '*');
 console.log(bookCreate.getInfo());
 console.log(bookCreate.getPrice());
+console.log(bookCreate.addRating());
