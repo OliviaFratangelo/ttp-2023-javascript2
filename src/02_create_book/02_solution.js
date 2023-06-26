93b3e0cafde8d60ab8f73245fc9f4add46a8496c
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 18:46:07 -0400

    solution attempt 1 create book

--- src/02_create_book/02_solution.js
+++ src/02_create_book/02_solution.js
@@ -1,2 +1,18 @@
 // Code your solution here
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function createBook(id, title, author, price, rating) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.price = price;
+  let rating = ['*', '*', '*', '*', '*'];
+  this.getInfo = function () {
+    return `${this.title} by ${this.author}`; }
+  this.getPrice = function() {
+    return `${this.price}`; }
+  };
+
+let book 1 = new createBook(1, 'The Great Gatsby','F. Scott Fitzgerald', '$20', ['*','*'])
+createBook.prototype.getInfo(title, author);
+createBook.prototype.getPrice(price);
+
