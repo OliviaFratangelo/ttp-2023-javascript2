f74d8fe2987b482ed08212d2c4a9dcf517d028fa
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 13:18:09 -0400

    multiplication table solution

--- src/00_multiplication_table/00_solution.js
+++ src/00_multiplication_table/00_solution.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function multiplicationTable(rows, columns) {
+  let table = [];
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= columns; j++) {
+      table.push(i * j);
+    }
+  }
+  return table;
+}
+console.log(multiplicationTable(3, 5));
