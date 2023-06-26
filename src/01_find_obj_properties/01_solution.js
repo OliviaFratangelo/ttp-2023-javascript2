668fb187a9b901c9455157debc97357632e9d37d
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 18:10:39 -0400

    finding object property solution

--- src/01_find_obj_properties/01_solution.js
+++ src/01_find_obj_properties/01_solution.js
@@ -1,1 +1,15 @@
 // Code your solution here
+let findObjPropsHasOwn = function (object) {
+  let properties = [];
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      properties.push(key);
+    }
+  }
+  return properties.join(', ');
+};
+let person = new Object();
+person.name = 'John';
+person.age = '23';
+person.city = 'NY';
+console.log(findObjPropsHasOwn(person));
