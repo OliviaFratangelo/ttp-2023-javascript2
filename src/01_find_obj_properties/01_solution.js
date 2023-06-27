bb850f62e6f69f90a91c16f5cec0a5a988ae52f7
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 21:15:19 -0400

    added comments

--- src/01_find_obj_properties/01_solution.js
+++ src/01_find_obj_properties/01_solution.js
@@ -1,20 +1,24 @@
 // Code your solution here
 let findObjPropsHasOwn = function (object) {
-  let properties = [];
+  //create function with an object as its argument
+  let properties = []; //initialize an empty array to store properties of each object
   for (let key in object) {
+    //for loop to go through each key in the object
     if (object.hasOwnProperty(key)) {
-      properties.push(key);
+      //checks if the object has the key as its property
+      properties.push(key); //pushes the key into the properties array
     }
   }
-  return properties.join(', ');
+  return properties.join(', '); //returns keys that are owned/properties as a comma separated list
 };
-let person = new Object();
-person.name = 'John';
-person.age = '23';
-person.city = 'NY';
-console.log(findObjPropsHasOwn(person));
+let person = new Object(); // creates a new object called person
+person.name = 'John'; //person has a name property
+person.age = '23'; //person has an age property
+person.city = 'NY'; //person has a city property
+console.log(findObjPropsHasOwn(person)); //returns the values of the properties
 
 let findObjKeys = function (object) {
+  //function to return the names of the properties
   let properties = Object.keys(object);
   return properties.join(', ');
 };
