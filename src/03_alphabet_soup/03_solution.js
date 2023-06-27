ff8aec15ed8c34e967906a97ad7e65d2f7f537b3
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-06-26 20:42:10 -0400

    fixing else statement

--- src/03_alphabet_soup/03_solution.js
+++ src/03_alphabet_soup/03_solution.js
@@ -2,8 +2,22 @@
 let soup = function (phrase, bank) {
   if (bank.length < phrase.length) {
     return false;
-  }
-  elif (bank.length >= phrase.length) {
+  } else {
+    for (let x = 0; x < phrase.length; x++) {
+      continue;
+      for (let y = 0; y < bank.length; y++) {
+        if (phrase[x] === bank[y]) {
+          let check = true;
+          break;
+        }
+      }
+      if (check) {
+        return true;
+      }
+    }
     return true;
   }
 };
+phrase = 'hello';
+bank = 'jakdksldsfo';
+console.log(soup(phrase, bank));
